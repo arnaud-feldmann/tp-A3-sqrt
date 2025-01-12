0116af94473ecee21a6e7cbc7ef3113ac79a6db6
arnaud-feldmann <arnaud.feldmann@gmail.com>
2025-01-12 20:29:40 +0100

    ajout init uc

--- uc.vhd
+++ uc.vhd
@@ -12,12 +12,13 @@ entity uc is
         start : in std_logic;
         raz : in std_logic;
         clk : in std_logic;
+        init : out std_logic;
         done : out std_logic
     );
 end entity;
 
 architecture uc_behavorial of uc is
-    type etat is (ATTENDRE, INIT, CALC, FIN);
+    type etat is (ATTENDRE, INITIALISATION, CALC, FIN);
     signal present, futur : etat;
     signal i : unsigned(n-1 downto 0);
 begin
@@ -25,8 +26,8 @@ begin
     begin
         futur <= present;
         case present is
-            when ATTENDRE => if start = '1' or start = 'H' then futur <= INIT; else futur <= ATTENDRE; end if;
-            when INIT => futur <= CALC;
+            when ATTENDRE => if start = '1' or start = 'H' then futur <= INITIALISATION; else futur <= ATTENDRE; end if;
+            when INITIALISATION => futur <= CALC;
             when CALC => if i = 0 then futur <= FIN; else futur <= CALC; end if;
             when FIN => if start = '1' or start = 'H' then futur <= FIN; else futur <= ATTENDRE; end if;
         end case;
@@ -36,15 +37,18 @@ begin
     begin
         if raz then
             present <= ATTENDRE;
+            init <= '0';
+            done <= '0';
         elsif rising_edge(clk)
         then
             present <= futur;
             case present is
                 when ATTENDRE => done <= '0';
-                when INIT => i <= to_unsigned(n-1,n);
+                when INITIALISATION => init <= '1';i <= to_unsigned(n-1,n);
                 when CALC =>
                     i_temp := i - 1;
                     i <= i_temp;
+                    init <= '0';
                 when FIN => done <= '1';
             end case;
         end if;
