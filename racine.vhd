d1ca8364640f0e478d890588fa6aa89c12cf9541
arnaud-feldmann <arnaud.feldmann@gmail.com>
2025-01-18 17:55:59 +0100

    simplification opérations

--- racine.vhd
+++ racine.vhd
@@ -53,9 +53,9 @@ begin
                 when INIT => i <= to_unsigned(n-1,n); D <= unsigned(A); R <= (others => '0'); Z <= (others => '0');
                 when CALC =>
                     if R >= 0 then
-                        R_temp := shift_left(R,2) + signed(resize(shift_right(D, 2*n - 2), R'length)) - shift_left(resize(Z, R'length), 2) - 1;
+                        R_temp := shift_left(R,2) + signed(resize(shift_right(D, 2*n - 2), R'length)) - signed(resize(Z, R'length - 2) & "01");
                     else
-                        R_temp := shift_left(R,2) + signed(resize(shift_right(D, 2*n - 2), R'length)) + shift_left(resize(Z, R'length), 2) + 3;
+                        R_temp := shift_left(R,2) + signed(resize(shift_right(D, 2*n - 2), R'length)) + signed(resize(Z, R'length - 2) & "11");
                     end if;
                     if R_temp >= 0 then
                         Z_temp := shift_left(Z, 1) + 1;
