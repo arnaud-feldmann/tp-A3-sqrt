8f1402af70b459e6cd7dc194448091555b841b9d
arnaud-feldmann <arnaud.feldmann@gmail.com>
2025-01-19 22:12:29 +0100

    Revert "additionneur à retenue anticipée"
    This reverts commit 65f37d1a24e48b8eaa664e1257a00f5ff4e15743.

--- additionneur.vhd
+++ additionneur.vhd
@@ -16,22 +16,21 @@ entity additionneur is
 end entity;
 
 architecture additionneur_archi of additionneur is
-    signal resultat_prec_v, retenue_anticipee : std_logic_vector(n downto 0);
+    signal resultat_prec_v, retenue_prec_v : std_logic_vector(n downto 0);
 begin
     resultat_prec_v(0) <= '0';
-    retenue_anticipee(0) <= '0';
+    retenue_prec_v(0) <= '0';
     g : for i in n - 1 downto 0
     generate
-	     retenue_anticipee(i + 1) <= (x(i) and y(i)) or ((x(i) or y(i)) and retenue_anticipee(i));
         u : entity work.additionneur_cascadable port map
         (
             x => x(i),
             y => y(i),
-            retenue_prec => retenue_anticipee(i),
+            retenue_prec => retenue_prec_v(i),
             resultat => resultat_prec_v(i + 1),
-            retenue => retenue_anticipee(i + 1)
+            retenue => retenue_prec_v(i + 1)
         );
     end generate;
     resultat <= resultat_prec_v(n downto 1);
-    retenue <= retenue_anticipee(n);
+    retenue <= retenue_prec_v(n);
 end additionneur_archi;
